69af33ff641cad46acb129add1b5fef48187fae4
Fabiano Lira <fabianolira@cge.pi.gov.br>
2025-06-05 09:59:49 -0300

    alterando fiados3

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Aplicando migrations com Alembic..."
 
-alembic upgrade head
+#alembic upgrade head
 
 echo "Iniciando servidor FastAPI..."
 exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
